4851b3899b1f7f62cce8b02ccf607d390b71b586
panding1987 <913329364@qq.com>
2017-07-03 19:48:31 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -23,44 +23,35 @@ print_title() { #{{{
 arch_chroot() { #{{{
 
 	arch-chroot /mnt /bin/bash -c "${1}"
-
 }
 
 #}}}
 
-pause_function() { #{{{
-
-	print_line
-
-	read -e -sn 1 -p "Press enter to continue..."
-
-
-} #}}}
-
 update_mirrorlist(){
-	print_title "update_mirrorlist"
+	print_title "替换仓库列表"
 	tmpfile=$(mktemp --suffix=-mirrorlist)	
 	url="https://www.archlinux.org/mirrorlist/?country=CN&protocol=http&protocol=https&ip_version=4"
 	curl -so ${tmpfile} ${url} 
 	sed -i 's/^#Server/Server/g' ${tmpfile}
-	mv -i ${tmpfile} /etc/pacman.d/mirrorlist;
+	mv -f ${tmpfile} /etc/pacman.d/mirrorlist;
 }
 create_partitions(){
-	print_title "create_partion"
+	print_title "开始分区"
 	parted -s /dev/sda mklabel msdos
 	parted -s /dev/sda mkpart primary ext4 1M 1G
-	parted -s /dev/sda mkpart primary ext4 1G 4G
-	parted -s /dev/sda mkpart primary ext4 4G 100%
+	parted -s /dev/sda mkpart primary ext4 1G 20G
+	parted -s /dev/sda mkpart primary ext4 20G 100%
 	parted -s /dev/sda set 1 boot on
 	parted -s /dev/sda print
 }
 format_partitions(){
+	print_title "开始格式化"
 	mkfs.ext4 /dev/sda1 
 	mkfs.ext4 /dev/sda2 
 	mkfs.ext4 /dev/sda3 
 }
 mount_partitions(){
-	print_title "mount_partitions"
+	print_title "挂载分区"
 	mount /dev/sda2 /mnt
 	mkdir /mnt/boot
 	mount /dev/sda1 /mnt/boot
@@ -70,29 +61,24 @@ mount_partitions(){
 }
 
 install_baseSystem(){
-	print_title "install_baseSystem"
-	pacstrap -i /mnt base base-devel iw wpa_supplicant dialog netctl vim grub
+	print_title "最小安装"
+	pacstrap /mnt base base-devel iw wpa_supplicant dialog netctl vim grub screenfetch
 }
 
 generate_fstab(){
-	print_title "generate_fstab"
+	print_title "生成标卷文件表"
 	genfstab -U /mnt >> /mnt/etc/fstab
 }
 
 configure_system(){
-	print_title "config_time"
+	print_title "配置系统时间地区"
 	arch_chroot "ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/locatime"
 	arch_chroot "hwclock --systohc --utc"
 	arch_chroot "mkinitcpio -p linux"
-	clear
-	print_title "set root passwd"
-	passwd
-
-	
 }
 configure_hostname(){
 
-	print_title "HOSTNAME - https://wiki.archlinux.org/index.php/HOSTNAME"
+	print_title "添加本地域名"
 
 	read -p "Hostname [ex: archlinux]: " host_name
 
@@ -115,21 +101,22 @@ configure_hostname(){
 }
 
 configrue_bootloader(){
-	print_title "configure_bootloader"
+	print_title "安装配置引导程序"
 	arch_chroot "grub-install --target=i386-pc /dev/sda"
 	arch_chroot "grub-mkconfig -o /boot/grub/grub.cfg"
 	umount -R /mnt
-	pause_function
+	clear
+	print_title "install has been.please reboot ."
 }
 
 
-#update_mirrorlist
-#create_partitions
-#format_partitions
-#mount_partitions
-#install_baseSystem
-#generate_fstab
-#configure_system
-#configure_hostname
+update_mirrorlist
+create_partitions
+format_partitions
+mount_partitions
+install_baseSystem
+generate_fstab
+configure_system
+configure_hostname
 configrue_bootloader
 
