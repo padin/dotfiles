388a2f6c3757a5bb7578ff17514e4a80b68b2627
潘鼎 <913329364@qq.com>
2017-07-03 22:20:20 +0800

    Update setup.sh
    update

--- setup.sh
+++ setup.sh
@@ -27,16 +27,18 @@ arch_chroot() { #{{{
 
 #}}}
 
+#替换仓库列表
 update_mirrorlist(){
-	print_title "替换仓库列表"
+	print_title "update_mirrorlist"
 	tmpfile=$(mktemp --suffix=-mirrorlist)	
 	url="https://www.archlinux.org/mirrorlist/?country=CN&protocol=http&protocol=https&ip_version=4"
 	curl -so ${tmpfile} ${url} 
 	sed -i 's/^#Server/Server/g' ${tmpfile}
 	mv -f ${tmpfile} /etc/pacman.d/mirrorlist;
 }
+#开始分区
 create_partitions(){
-	print_title "开始分区"
+	print_title "create_partitions"
 	parted -s /dev/sda mklabel msdos
 	parted -s /dev/sda mkpart primary ext4 1M 1G
 	parted -s /dev/sda mkpart primary ext4 1G 20G
@@ -44,14 +46,16 @@ create_partitions(){
 	parted -s /dev/sda set 1 boot on
 	parted -s /dev/sda print
 }
+#开始格式化
 format_partitions(){
-	print_title "开始格式化"
+	print_title "format_partitions"
 	mkfs.ext4 /dev/sda1 
 	mkfs.ext4 /dev/sda2 
 	mkfs.ext4 /dev/sda3 
 }
+#挂载分区
 mount_partitions(){
-	print_title "挂载分区"
+	print_title "mount_partitions"
 	mount /dev/sda2 /mnt
 	mkdir /mnt/boot
 	mount /dev/sda1 /mnt/boot
@@ -59,26 +63,27 @@ mount_partitions(){
 	mount /dev/sda3 /mnt/home
 	lsblk
 }
-
+#最小安装
 install_baseSystem(){
-	print_title "最小安装"
+	print_title "install_baseSystem"
 	pacstrap /mnt base base-devel iw wpa_supplicant dialog netctl vim grub screenfetch
 }
-
+#生成标卷文件表
 generate_fstab(){
-	print_title "生成标卷文件表"
+	print_title "generate_fstab"
 	genfstab -U /mnt >> /mnt/etc/fstab
 }
-
+#配置系统时间地区
 configure_system(){
-	print_title "配置系统时间地区"
+	print_title "configure_system"
 	arch_chroot "ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/locatime"
 	arch_chroot "hwclock --systohc --utc"
 	arch_chroot "mkinitcpio -p linux"
 }
+#添加本地域名
 configure_hostname(){
 
-	print_title "添加本地域名"
+	print_title "configure_hostname"
 
 	read -p "Hostname [ex: archlinux]: " host_name
 
@@ -99,9 +104,9 @@ configure_hostname(){
 	arch_chroot "sed -i '/::1/s/$/ '${host_name}'/' /etc/hosts"
 
 }
-
+#安装配置引导程序
 configrue_bootloader(){
-	print_title "安装配置引导程序"
+	print_title "configrue_bootloader"
 	arch_chroot "grub-install --target=i386-pc /dev/sda"
 	arch_chroot "grub-mkconfig -o /boot/grub/grub.cfg"
 	umount -R /mnt
